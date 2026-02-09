bc27ee69aa58aa40d37deca5f756110fcbd57b65
Yashasvi Dagar <dagaryashu2046@gmail.com>
2026-02-10 01:45:13 +0530

    ADDING EXTERNAL LIBRARIES AND MAKING CHECKOUT INTERACTIVE

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,14 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
-//localStorage only gives the string as the return type but JSON will convert it back to the array
+export let cart = JSON.parse(localStorage.getItem('cart'));//localstorage gives only the string type but JSON gives the array
 
-if (!cart) {//this gives the default value...
+if (!cart) {//gives the default value
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
+    deliveryOptionId: '1'
   }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 
@@ -29,7 +30,8 @@ export function addToCart(productId) {
   } else {
     cart.push({
       productId: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     });
   }
 
@@ -46,6 +48,20 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
